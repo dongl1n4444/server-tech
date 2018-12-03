5f367c20c3617209579515e3dc2e205e58be4236
twenty0ne <twenty0ne@qq.com>
2018-12-03 14:11:44 +0800

    [admin-web]update

--- admin-web/app.js
+++ admin-web/app.js
@@ -12,6 +12,7 @@ nunjucks.configure('views', {
 app.set('view engine', 'njk');
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'node_modules')));
 app.use('/', routes);
 
 app.listen(3000, function() {
